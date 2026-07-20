8725fff7c4ca05b20142037286b599cae274dd1b
Garima Garg <117982650+garimagarg19@users.noreply.github.com>
2026-07-20 19:13:08 +0530

    Time: 15 ms (70.46%), Space: 58.9 MB (26.94%) - LeetHub

---
+++ 0018-4sum/0018-4sum.js
@@ -0,0 +1,51 @@
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number[][]}
+ */
+function fourSum(nums, target) {
+    nums.sort((a, b) => a - b);
+
+    const result = [];
+    const n = nums.length;
+
+    for (let i = 0; i < n - 3; i++) {
+        // Skip duplicates for first number
+        if (i > 0 && nums[i] === nums[i - 1]) continue;
+
+        for (let j = i + 1; j < n - 2; j++) {
+            // Skip duplicates for second number
+            if (j > i + 1 && nums[j] === nums[j - 1]) continue;
+
+            let left = j + 1;
+            let right = n - 1;
+
+            while (left < right) {
+                const sum =
+                    nums[i] + nums[j] + nums[left] + nums[right];
+
+                if (sum === target) {
+                    result.push([
+                        nums[i],
+                        nums[j],
+                        nums[left],
+                        nums[right]
+                    ]);
+
+                    // Skip duplicates
+                    while (left < right && nums[left] === nums[left + 1]) left++;
+                    while (left < right && nums[right] === nums[right - 1]) right--;
+
+                    left++;
+                    right--;
+                } else if (sum < target) {
+                    left++;
+                } else {
+                    right--;
+                }
+            }
+        }
+    }
+
+    return result;
+}
